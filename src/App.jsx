8cfe02a765afdda9dd071a0dca113ccfb4b5eedc
Ruben Amarilla <daniel.amarilla.rda@gmail.com>
2023-01-18 10:22:40 -0300

    edicion final

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello Dojo!</h1>
+    </div>
+  );
+}
+export default App;
---
+++ src/ListaTareas.jsx
@@ -0,0 +1,14 @@
+function ListaTareas() {
+    return (
+        <div>
+            <h2>Things I need to do:</h2>
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>
+            </ul>
+        </div>
+    );
+}
+export default ListaTareas;
